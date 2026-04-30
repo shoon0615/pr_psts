78f85da70cc4ba5b2ff59123e25f1fa84490d9a2
shoon0615 <67447628+shoon0615@users.noreply.github.com>
2026-05-01 01:25:30 +0900

    [20260430] devcontainer 추가 (#14) (#19)
    [로컬] .env 추가
    # Database Arguments
    DATABASE_USER=side
    DATABASE_PASSWORD=project
    DATABASE_DB=side_project
    
    Co-authored-by: shoon0615 <shoon0615@github.com>

--- .devcontainer/commands/initializeCommand.sh
+++ .devcontainer/commands/initializeCommand.sh
@@ -6,8 +6,16 @@ containerWorkspaceFolder=$2
 localWorkspaceFolderBasename=$3
 containerWorkspaceFolderBasename=$4
 
-# Create env for docker-compose
-cat > .devcontainer/.env <<EOL
+ENV_FILE=".devcontainer/.env"
+
+# 이미 .env 가 있으면 건드리지 않음
+if [ -f "$ENV_FILE" ]; then
+  echo "$ENV_FILE already exists. skip."
+  exit 0
+fi
+
+# .env가 없을 때만 생성
+cat > "$ENV_FILE" <<EOL
 
 # Original DevContainer Arguments
 localWorkspaceFolder=$localWorkspaceFolder
