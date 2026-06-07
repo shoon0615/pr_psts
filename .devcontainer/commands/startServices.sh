d6dd29ffee7f6d89bb57e7d3be1f56d5919bc64e
shoon0615 <shoon0615@github.com>
2026-06-08 04:26:56 +0900

    [20260608] 마무리

--- .devcontainer/commands/startServices.sh
+++ .devcontainer/commands/startServices.sh
@@ -4,3 +4,4 @@ set -e  # 중간에 하나라도 실패 시, 즉시 스크립트 종료(디버�
 docker compose --profile db up -d
 docker compose --profile search up -d
 docker compose --profile message up -d
+docker compose --profile cypress up -d
